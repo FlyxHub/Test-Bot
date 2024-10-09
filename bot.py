209e7f8ed782b25a13ea0272a600a9197b5fa651
FlyxHub <jphochstatter@gmail.com>
2024-10-09 17:26:35 -0500

    Added Rock Paper Scissors game

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # Import required libraries
 import discord
 from discord.ext import commands
+import random
 
 # Read token from ./token.txt
 with open("token.txt", "r") as tokenFile:
@@ -37,6 +38,62 @@ async def whoami(ctx) -> None:
 async def ping(ctx) -> None:
     await ctx.send(f":ping_pong: *Pong!* Bot ping: {int(bot.latency*1000)} MS")
     return
+
+
+# Rock Paper Scissors game
+@bot.command()
+async def rps(ctx, choice: str) -> None:
+    choice = choice.lower()
+    options = ["rock", "paper", "scissors"]
+
+    botPick = random.choice(options)
+
+    if choice not in options:
+        await ctx.send('You must pick either "rock", "paper", or "scissors"!')
+        return
+    else:
+        pass
+
+    if choice == botPick:
+        await ctx.send(f"**{botPick.title()}!** It's a tie!")
+        return
+    else:
+        pass
+
+    while choice == "rock":
+        if botPick == "paper":
+            await ctx.send(f"**{botPick.title()}!** You lose!")
+            return
+        else:
+            await ctx.send(f"**{botPick.title()}!** You win!")
+            return
+
+    while choice == "paper":
+        if botPick == "rock":
+            await ctx.send(f"**{botPick.title()}!** You win!")
+            return
+        else:
+            await ctx.send(f"**{botPick.title()}!** You lose!")
+            return
+
+    while choice == "scissors":
+        if botPick == "rock":
+            await ctx.send(f"**{botPick.title()}!** You win!")
+            return
+        else:
+            await ctx.send(f"**{botPick.title()}!** You lose!")
+            return
+
+
+# Error handler for rps command
+@rps.error
+async def rpsError(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('You must pick either "rock", "paper", or "scissors"!')
+        return
+    else:
+        await ctx.send(f"An error occurred. Try again later.")
+        return
 
 
 # Run the bot after defining commands and listeners
